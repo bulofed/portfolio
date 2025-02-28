d0c035db644c37fdd81a264916c708024a870ac5
Loïc Butor <pro.butor.loic@gmail.com>
2025-02-28 13:25:12 +0100

    maj

---
+++ app/components/ProjectList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Project from './Project';
+
+const projects = [
+    {
+      href: "https://github.com/bulofed/Astra",
+      imgSrc: "/astra.png",
+      description: "Astra is an isometrical RPG game where the goal is to beat every level and get to the end of the game. You beat the levels by killing every enemies in the level. The game is made in Python, using the PyGame FOSS library and is a school project."
+    },
+    {
+      href: "https://github.com/bulofed/Cryptr",
+      imgSrc: "/cryptr.png",
+      description: "Cryptr is a web escape game with multiple levels. The goal is to escape the room by solving puzzles. The game is made with Nuxt and MongoDB."
+    },
+    {
+      href: "",
+      imgSrc: "/dungeons.png",
+      description: "DungeonVR is a VR game where you have to escape a dungeon. I have worked on this project with a team of 5 people using Unreal Engine. I've made the locomotion system (snap-turn, move towards head and joystick mode) as well as the grabbing system."
+    }
+  ];
+
+const ProjectList = () => {
+    return (
+        <>
+            <h2 className='font-bold text-5xl text-center py-6'>Projects</h2>
+            {projects.map((project, index) => (
+                <Project 
+                key={index}
+                href={project.href}
+                imgSrc={project.imgSrc}
+                description={project.description}
+                />
+            ))}
+        </>
+    );
+};
+
+export default ProjectList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,14 @@
 import React from 'react';
 import Introduction from './components/Introduction';
 import Video from './components/Video';
-import Project from './components/Project';
-
-const projects = [
-  {
-    href: "https://github.com/bulofed/Astra",
-    imgSrc: "/astra.png",
-    description: "Astra is an isometrical RPG game where the goal is to beat every level and get to the end of the game. You beat the levels by killing every enemies in the level. The game is made in Python, using the PyGame FOSS library and is a school project."
-  },
-  {
-    href: "https://github.com/bulofed/Cryptr",
-    imgSrc: "/cryptr.png",
-    description: "Cryptr is a web escape game with multiple levels. The goal is to escape the room by solving puzzles. The game is made with Nuxt and MongoDB."
-  },
-  {
-    href: "",
-    imgSrc: "/dungeons.png",
-    description: "DungeonVR is a VR game where you have to escape a dungeon. I have worked on this project with a team of 5 people using Unreal Engine. I've made the locomotion system (snap-turn, move towards head and joystick mode) as well as the grabbing system."
-  }
-];
+import ProjectList from './components/ProjectList';
 
 const Page = () => {
   return (
     <>
       <Introduction />
       <Video />
-      {projects.map((project, index) => (
-        <Project 
-          key={index}
-          href={project.href}
-          imgSrc={project.imgSrc}
-          description={project.description}
-        />
-      ))}
+      <ProjectList />
     </>
   );
 };
